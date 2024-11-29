e3c16cc131a732195b4cad752cb5ce865c0451fa
huangjunrong <jrlingdu888@gmail.com>
2024-11-29 18:14:21 +0800

    modify

---
+++ script.js
@@ -0,0 +1,3 @@
+function iosDown() {
+    window.location.href = 'itms-services:///?action=download-manifest&url=itms-services:///?action=download-manifest&url=https://soft.shanchen.com/youziip/ceshi.plist';
+}
